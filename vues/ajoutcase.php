b265ac5c701ac65c58fe4a8b9f54193eb69af77b
Latouille Mélissa <melissa.latouille@etu.unilim.fr>
2018-06-12 12:01:48 +0200

    update ajout case
    css ok
    téléchargement template vide ok

--- vues/ajoutcase.php
+++ vues/ajoutcase.php
@@ -21,27 +21,28 @@
 		</div>
 
 		<div id="templatevide">
+			<div id="template_select">
+				<img class="apercu"/>
+			</div>
 			<div class="icones">
-
 				<div class="alignleft">
 					<span id="choix_formes">
 						<?php include 'modals/choixformes.php';?>
 					</span>
-					<span id="telecharger_template"> 
+					<a id="telecharger_template">
 						<img class="icone" src="../img/telecharger-template.png" alt="Télécharger template" title="Télécharger" width="40px" />
-					</span>
-					<span id="ajouter_case">
+					</a>
+					<button id="ajouter_case">
 						<img class="icone" src="../img/ajouter-case.png" alt="Ajouter la case" title="Ajouter" width="40px" />
-					</span>
+					</button>
 				</div>
-
 				<div class="alignright">
-					<span id="valider_case">
+					<button id="valider_case">
 						<img class="icone" src="../img/valider-case.png" alt="Validation" title="Valider" width="40px" />
-					</span>
-					<span id="annulation">
+					</button>
+					<button id="annulation">
 						<img class="icone" src="../img/annulation.png" alt="Annulation" title="Annuler" width="40px" />
-					</span>
+					</button>
 				</div>
 
 			</div>
--- vues/modals/choixformes.php
+++ vues/modals/choixformes.php
@@ -3,42 +3,45 @@
 </button>
 
 <div id="formes-modal" style="display: none">
-	<fieldset>
-		<legend>Choisissez la forme de votre case :</legend>
-		<form class="templates">
-	  	<label for="template_1" class="verticalalign">
-	  		<img class="template" src="../img/templates/template1.jpg" height="60%">
-	  		<input type='radio' id="template_1" class="hidden"/>
-	  	</label>
-	    <label for="template_2" class="verticalalign">
-	    	<img class="template" src="../img/templates/template2.jpg" height="60%">
-	    	<input type='radio' id="template_2" class="hidden"/>
-	    </label>
-	    <label for="template_3" class="verticalalign">
-	    	<img class="template" src="../img/templates/template3.jpg" height="60%">
-	    	<input type='radio' id="template_3" class="hidden"/>
-	    </label>
-	    <label for="template_4" class="verticalalign">
-	    	<img class="template" src="../img/templates/template4.jpg" height="60%">
-	    	<input type='radio' id="template_4" class="hidden"/>
-	    </label>
-	    <label for="template_5">
-	    	<img class="template" src="../img/templates/template5.jpg" height="90%">
-	    	<input type='radio' id="template_5" class="hidden" />
-	    </label>
-	  </form>
-	</fieldset>
+		<h3>Choisissez la forme de votre case :</h3>
+		<div class="templates">
+	  		<img id="template1.jpg" class="template" src="../img/templates/template1.jpg"/>
+	    	<img id="template2.jpg" class="template" src="../img/templates/template2.jpg"/>
+	    	<img id="template3.jpg" class="template" src="../img/templates/template3.jpg"/>
+	    	<img id="template4.jpg" class="template" src="../img/templates/template4.jpg"/>
+	    	<img id="template5.jpg" class="template" src="../img/templates/template5.jpg"/>
+	  </div>
   <button type="button" class="close">x</button>
   <div class="arrow-up"></div>
 </div>
 
 <script>
-	
 		$('.bouton_formes').click(function() {
 			$("#formes-modal").toggle('slow');
 		});
 		$('.close').click(function() {
 			$("#formes-modal").hide('slow');
 		});
+
+	$(function() {
+		$('.template').css('border', '5px white solid');
+		$('.template').click(function() {
+			$('.template').css('border','5px white solid');
+			$(this).css('border','5px #3e5164 solid');
+			// Récupère l'id des radio qui sont les src
+			var src = $(this).attr('src');
+			var id = $(this).attr('id');
+			var alt = $(this).attr('class');
+			// Modifier img apercu
+			$(".apercu").attr({
+				src: src,
+				id: id,
+				alt: alt});
+			$("#telecharger_template").attr({
+				href: src,
+				download: id});
+		});
+	});
+
 </script>
 
